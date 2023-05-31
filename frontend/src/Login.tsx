50eb3715e222484a8074956f1a73bc7a7be6fbe1
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2023-05-31 22:04:41 +0300

    added files

---
+++ frontend/src/Login.tsx
@@ -0,0 +1,16 @@
+import './Login.css'
+
+function Login() {
+
+  return (
+    <>
+        <form action="">
+            <input type="text" placeholder='Username'  />
+            <input type="text" placeholder='Password' />
+            <div className='bg-red-500'>a</div>
+        </form>
+    </>
+  )
+}
+
+export default Login
