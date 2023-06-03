300f74ad952b646810dc0ace1d451182769a6bfe
911-Bejan-Andrei <andrei.bejan1@stud.ubbcluj.ro>
2023-06-04 02:32:53 +0300

    added crud operations for admin and user

--- src/main/java/com/example/BucketList/controller/UserController.java
+++ src/main/java/com/example/BucketList/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.BucketList.controller;
 
+import com.example.BucketList.domain.Destination;
+import com.example.BucketList.domain.Role;
 import com.example.BucketList.domain.User;
 import com.example.BucketList.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +10,101 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/save")
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.OK);
+    @PostMapping("/user/{userId}/destination")
+    public ResponseEntity<String> saveUserPrivateDestination(@PathVariable Long userId, @RequestBody Destination destination) {
+        User user = userService.getSingleUser(userId);
+        if(user != null) {
+            List<Role> roles = user.getRoles();
+            for (Role r : roles) {
+                if (Objects.equals(r.getName(), "USER")) {
+                    return new ResponseEntity<>(userService.savePrivateDestination(user, destination), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(userService.savePrivateDestination(user, destination), HttpStatus.UNAUTHORIZED);
     }
+
+    @PostMapping("/admin/{adminId}/destination")
+    public ResponseEntity<String> saveAdminPublicDestination(@PathVariable Long adminId, @RequestBody Destination destination) {
+        User user = userService.getSingleUser(adminId);
+        if(user != null) {
+            List<Role> roles = user.getRoles();
+            for (Role r : roles) {
+                if (Objects.equals(r.getName(), "ADMIN")) {
+                    return new ResponseEntity<>(userService.savePublicDestination(destination), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(userService.savePublicDestination(destination), HttpStatus.UNAUTHORIZED);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        User user = userService.getSingleUser(userId);
+        if(user != null) {
+            List<Role> roles = user.getRoles();
+            for (Role r : roles) {
+                if (Objects.equals(r.getName(), "USER")) {
+                    return new ResponseEntity<>(userService.deleteUser(userId), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(userService.deleteUser(userId), HttpStatus.UNAUTHORIZED);
+    }
+
+    @DeleteMapping("/admin/{adminId}/destination/{destinationId}")
+    public ResponseEntity<String> deletePublicDestination(@PathVariable Long adminId, @PathVariable Long destinationId) {
+        User user = userService.getSingleUser(adminId);
+        if(user != null) {
+            List<Role> roles = user.getRoles();
+            for (Role r : roles) {
+                if (Objects.equals(r.getName(), "ADMIN")) {
+                    return new ResponseEntity<>(userService.deletePublicDestination(destinationId), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(userService.deletePublicDestination(destinationId), HttpStatus.UNAUTHORIZED);
+    }
+
+    @DeleteMapping("/user/{userId}/destination/{destinationId}")
+    public ResponseEntity<String> deletePrivateDestination(@PathVariable Long userId, @PathVariable Long destinationId) {
+        User user = userService.getSingleUser(userId);
+        if(user != null) {
+            List<Role> roles = user.getRoles();
+            for (Role r : roles) {
+                if (Objects.equals(r.getName(), "USER")) {
+                    return new ResponseEntity<>(userService.deletePrivateDestination(user, destinationId), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(userService.deletePrivateDestination(user, destinationId), HttpStatus.UNAUTHORIZED);
+    }
+
+    /*@PutMapping("/user/{userId}/changePassword")
+    public ResponseEntity<String> updatePassword(@PathVariable Long userId, @) {
+        User user = userService.getSingleUser(userId);
+        if(user != null) {
+                return new ResponseEntity<>(userService.updatePassword(user, destination), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(userService.savePrivateDestination(user, destination), HttpStatus.UNAUTHORIZED);
+    }*/
 
     @GetMapping()
     public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getSingleUser(@PathVariable Long userId) {
-        return new ResponseEntity<User>(userService.getSingleUser(userId), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getSingleUser(userId), HttpStatus.OK);
     }
 }
