4af0e117e53f3fa6330e5213c58ace130d693bf7
911-Bejan-Andrei <andrei.bejan1@stud.ubbcluj.ro>
2023-06-04 22:32:24 +0300

    added cors mapping

--- src/main/java/com/example/BucketList/BucketListApplication.java
+++ src/main/java/com/example/BucketList/BucketListApplication.java
@@ -2,8 +2,11 @@ package com.example.BucketList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,5 +16,17 @@ public class BucketListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BucketListApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/login").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/destination").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/user").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 }
