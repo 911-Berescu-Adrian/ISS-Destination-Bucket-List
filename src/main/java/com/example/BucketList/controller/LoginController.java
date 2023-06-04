12aabb7f54e7edd71cbebd0859dbe7ebadb9444f
911-Bejan-Andrei <andrei.bejan1@stud.ubbcluj.ro>
2023-06-05 01:24:32 +0300

    this might fix the cors issues v3

--- src/main/java/com/example/BucketList/controller/LoginController.java
+++ src/main/java/com/example/BucketList/controller/LoginController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginController {
 
     @Autowired
