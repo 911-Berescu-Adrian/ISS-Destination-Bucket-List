b1ae4f6c1203f0d89431541365359a6f4106473b
911-Bejan-Andrei <andrei.bejan1@stud.ubbcluj.ro>
2023-06-05 01:29:45 +0300

    this might fix the cors issues v4

--- src/main/java/com/example/BucketList/BucketListApplication.java
+++ src/main/java/com/example/BucketList/BucketListApplication.java
@@ -1,37 +1,20 @@
 package com.example.BucketList;
 
+import com.example.BucketList.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
+@Import(CorsConfig.class)
 public class BucketListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BucketListApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-						.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("GET", "POST", "OPTIONS", "PUT")
-						.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-								"Access-Control-Request-Headers")
-						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-						.allowCredentials(true).maxAge(3600);
-			}
-		};
 	}
 }
